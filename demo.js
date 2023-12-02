b3e3eada0e8a4a57e670cb1ddc59997e1c7ce709
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-02 21:54:02 +0530

    Demo.js

---
+++ demo.js
@@ -0,0 +1,11 @@
+console.log("ans1zz");
+let btn = document.getElementById('click');
+btn.addEventListener("click",(e)=>{
+  btn.style.color="red"
+  btn.style.backgroundColor="black";
+})
+let btn1 = document.getElementById('click1');
+btn1.addEventListener("click",(e)=>{
+  btn1.style.color="blue"
+  btn1.style.backgroundColor="black";
+})

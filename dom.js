f566394cd4a6b3b4c3916a5fb44200607082fb90
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-03 14:09:55 +0530

    Chnaged list by using  tagname through js

--- dom.js
+++ dom.js
@@ -8,11 +8,22 @@ var additem = document.querySelector('.title');
 additem.style.color='green';
 additem.innerHTML='ADD ITEMS';
 
-
-let list = document.querySelectorAll(".list-group-item");
+// #GetelementmyClassname
+let list = document.getElementsByClassName("list-group-item");
 for(let i=0;i<list.length;i++){
     if(i==2){
         list[i].style.color="green";
     }
     list[i].style.fontWeight="bold";
+}
+
+
+//Getelemtnbytagname
+let tag = document.getElementsByTagName("li");
+for(let i=0;i<tag.length;i++){
+    if(i==2){
+        tag[i].style.color="red";
+    }
+    tag[i].style.fontWeight="Bold";
+    tag[i].style.backgroundColor="lightgrey";
 }

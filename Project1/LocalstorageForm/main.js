106ddc8bb906e0e88627b584e4ed250934c16e78
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-07 14:31:19 +0530

    Object Key value Pair Using JSON.stringify

--- Project1/LocalstorageForm/main.js
+++ Project1/LocalstorageForm/main.js
@@ -1,19 +1,28 @@
-console.log("122");
-localStorage.setItem("name",'sachin');
+console.log("Inside Main_JS");
+// localStorage.setItem("name",'sachin');
 
 let form = document.getElementById('my-form')
 form.addEventListener('submit',localstorage);
 
+localStorage.setItem("myobj",new Array())
 function localstorage(e){
     e.preventDefault();
     // console.log("Inside locale")
     let text = document.getElementById('name').value
     let email = document.getElementById('email').value;
+    let obj = {
+        name : text,
+        email : email
+    }
+    let string = JSON.stringify(obj);
     if(text.length>0 && email.includes('@') && email.length>1){
-        localStorage.setItem(text,email) ;
+        localStorage.setItem("myobj",string) ;
     }else{
         alert('Fields are not correctly filled')
     }
     
 }
+
+// 
+
 

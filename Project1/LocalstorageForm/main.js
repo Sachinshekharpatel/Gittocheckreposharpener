1ac32428ca9f119435b1ca1a33a1a4890b3b222f
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-07 18:23:17 +0530

    Edit Button Created

--- Project1/LocalstorageForm/main.js
+++ Project1/LocalstorageForm/main.js
@@ -25,19 +25,26 @@ function localstorage(e){
         localStorage.setItem(email,string) ;
         let items = document.querySelector('#items12');
         let btn = document.createElement('button');
-            btn.textContent = 'Delete'
+        btn.textContent = 'Delete'
+
+        let edit = document.createElement('button');
+        edit.textContent = 'EDIT';
         let li = document.createElement('li');
         li.classList = "item"
         li.textContent = email;
+
+
         btn.style.marginLeft='20px';
         btn.classList = "delete";
-       
-        li.appendChild(btn);
+        edit.style.marginLeft='20px';
+        edit.classList = "edit";
 
+        li.appendChild(btn);
+        li.appendChild(edit);
         items.appendChild(li);
 
         btn.addEventListener('click',deletefromlocalanddom);
-        
+        edit.addEventListener('click',editdetail)
     //    console.log(li);
     }else{
         alert('Fields are not correctly filled')
@@ -48,12 +55,41 @@ function localstorage(e){
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
    
 function deletefromlocalanddom(e){
-    e.preventDefault();
-    let items = document.querySelector('#items12');
-    // console.log(e.target.parentElement)
-       let todel = e.target.parentElement
-//    console.log(items.removeChild(todel));
-       items.removeChild(todel)
-    //    console.log(todel.firstChild.textContent);
-     localStorage.removeItem(todel.firstChild.textContent)
+            e.preventDefault();
+            let items = document.querySelector('#items12');
+            // console.log(e.target.parentElement)
+            let todel = e.target.parentElement
+            //    console.log(items.removeChild(todel));
+            items.removeChild(todel)
+            //    console.log(todel.firstChild.textContent);
+            localStorage.removeItem(todel.firstChild.textContent)
+}
+
+
+function editdetail(e){
+        e.preventDefault();
+        // console.log('inside editfunction');
+        let name = document.getElementById('name');
+        let email = document.getElementById('email');
+        
+        let items = document.querySelector('#items12');
+        //    console.log(e.target.parentElement)
+        let todel = e.target.parentElement
+        email.value = todel.firstChild.textContent 
+        let x = todel.firstChild.textContent
+        
+        let namevalue = JSON.parse(localStorage.getItem(x));
+        let y = namevalue.name ;
+        name.value = y ;
+        
+        
+        
+        
+        // console.log(items.removeChild(todel));
+        items.removeChild(todel)
+        // console.log(todel.firstChild.textContent);
+        localStorage.removeItem(todel.firstChild.textContent)
+
+        
+       
 }

54f23e0dddc11833e1aac7893eade954a2873b9b
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-07 11:35:07 +0530

    filter added

--- Project1/AddDeleteBtn/last.js
+++ Project1/AddDeleteBtn/last.js
@@ -32,10 +32,16 @@ var listitems = document.querySelectorAll('.list-group-item');
 function additemtoli(e){
     e.preventDefault();
     let text = document.getElementById("item").value
-    if(text.length>0){
+    let discription = document.getElementById("discription").value
+    
+    if(text.length>0 && discription.length>0){
         let newli = document.createElement('li');
+        let span = document.createElement('span');
+        span.textContent = " "+discription ;
+        span.className='span';
+        newli.appendChild(span);
         newli.className='list-group-item';
-        newli.textContent=text;
+        newli.textContent=text+" "+discription;
         // console.log(newli)
         let btn = document.createElement('button');
         btn.className ="btn btn-danger btn-sm float-right delete"
@@ -52,6 +58,28 @@ function additemtoli(e){
     }
    
     
+}
+
+// function filter && discription below of item list
+
+let filter = document.getElementById('filter');
+
+filter.addEventListener('keyup',filterbytext)
+
+function filterbytext (e){
+   let text = e.target.value.toLowerCase();
+   let list = document.querySelectorAll('li');
+   list.forEach(function(item){
+     let val = item.firstChild.textContent.toLowerCase();
+    
+     if(val.includes(text)){
+        
+        item.style.display = "block";
+
+     }else {
+        item.style.display = "none"        
+     }
+   })
 }
 
 

0f9edc5041caa5304b4ab327ee0e2a9cdad2556c
Sachinshekharpatel <heroft7024@gmail.com>
2023-12-30 13:38:56 +0530

    Update Functionality working On axios get post delete and update all are working

--- Project1/app1localstorageusednowaxiosworkwillbedone/index1.js
+++ Project1/app1localstorageusednowaxiosworkwillbedone/index1.js
@@ -7,7 +7,7 @@ let parentul = document.getElementById('parentul');
 
 function deletedata(id){
 //   console.log(id);
-  axios.delete(`https://crudcrud.com/api/0b23225eaa564f37a0ffc597a74a2781/usersdetails/${id}`)
+  axios.delete(`https://crudcrud.com/api/9609069fa4824d1cb18e41c1015a1afc/usersdetails/${id}`)
   .then(()=>{
     addtoDom();
   })
@@ -18,14 +18,14 @@ function deletedata(id){
 
 addtoDom()
 function addtoDom(){
-    axios.get('https://crudcrud.com/api/0b23225eaa564f37a0ffc597a74a2781/usersdetails')
+    axios.get('https://crudcrud.com/api/9609069fa4824d1cb18e41c1015a1afc/usersdetails')
     .then((res)=>{
-        console.log(res.data);
+        // console.log(res.data);
         parentul.innerHTML = null;
         let x = res.data;
         for(let i=0;i<x.length;i++){
             
-            let div = `<div><li>${x[i].value+"-"+x[i].discription+'-'+x[i].expense+" RS"}<button>Editdetail</button> <button onclick="deletedata('${x[i]._id}')"}'>DeleteItem</button></li></div>`
+            let div = `<div><li>${x[i].value+"-"+x[i].discription+'-'+x[i].expense+" RS"}<button onclick="editdetail('${x[i]._id}')">Editdetail</button> <button onclick="deletedata('${x[i]._id}')"}'>DeleteItem</button></li></div>`
             parentul.innerHTML = parentul.innerHTML + div ;
         }
     })
@@ -42,9 +42,9 @@ function addexpense(){
         expense : expense
     }
 
-    axios.post('https://crudcrud.com/api/0b23225eaa564f37a0ffc597a74a2781/usersdetails',obj)
+    axios.post('https://crudcrud.com/api/9609069fa4824d1cb18e41c1015a1afc/usersdetails',obj)
     .then((res)=>{
-        console.log(res.data) ;
+        // console.log(res.data) ;
         addtoDom();
     })
     .catch((err)=> console.log(err +"Inside Post request"))
@@ -54,6 +54,21 @@ function addexpense(){
    document.getElementById('expense').value  = null ;
 }
 
-function editdetail(){
+function editdetail(id){
+    document.getElementById('categorySelect').value
+    document.getElementById('discription').value
+    document.getElementById('expense').value
+    
+    axios.get(`https://crudcrud.com/api/9609069fa4824d1cb18e41c1015a1afc/usersdetails/${id}`)
+     .then((res)=>{
+    
+       let x = res.data;
+    //    console.log(x);
+       document.getElementById('categorySelect').value = null;
+       document.getElementById('discription').value = x.discription;
+       document.getElementById('expense').value = x.expense;
 
+       deletedata(id);
+      addtoDom();
+  })
 }

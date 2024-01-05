cf8db02ede619ef01c959d3262536017b060fd63
Sachinshekharpatel <heroft7024@gmail.com>
2024-01-05 15:22:03 +0530

    all functions  of crud working

--- CrudOperation/index.js
+++ CrudOperation/index.js
@@ -1,5 +1,6 @@
 // console.log("Inside Flipkart Type page");
 // console.log(window);
+// https://meet.google.com/qpn-kjup-eqz?pli=1
 
 async function addproduct(){
     let sellingprice = document.getElementById('sellingprice').value;
@@ -13,8 +14,23 @@ async function addproduct(){
     };
 
     try{
-        await axios.post("https://crudcrud.com/api/743c894ac2734eeebbb09e0a1733a084/product",obj);
-        location.reload()
+       
+        let response = await axios.post("https://crudcrud.com/api/214b2eca9dac46359efe5b1ddf9e5660/product", obj);
+ 
+        let id = response.data._id;
+        
+         
+        if(obj.category=='electronics'){
+            let append =`<li id="${id}">${obj.category}-${obj.sellingprice}-${obj.productname} <button onclick="deleteitem('${id}')">Delete</button></li>`;
+            electronicul.innerHTML = electronicul.innerHTML + append;
+        }else if(obj.category =='skincare'){
+            let append =`<li id="${id}">${obj.category}-${obj.sellingprice}-${obj.productname} <button onclick="deleteitem('${id}')">Delete</button></li>`;
+            skincareul.innerHTML = skincareul.innerHTML + append;
+        }else{
+            let append =`<li id="${id}">${obj.category}-${obj.sellingprice}-${obj.productname} <button onclick="deleteitem('${id}')">Delete</button></li>`;
+            fooditemul.innerHTML = fooditemul.innerHTML + append;
+        }
+
         document.getElementById('sellingprice').value = null;
         document.getElementById('productname').value =null;
         document.getElementById('categoryselect').value = null;
@@ -32,7 +48,7 @@ addtodom();
 
 async function addtodom(){
     try{
-        let res=await axios.get('https://crudcrud.com/api/743c894ac2734eeebbb09e0a1733a084/product');
+        let res=await axios.get('https://crudcrud.com/api/214b2eca9dac46359efe5b1ddf9e5660/product');
         // console.log(res.data);
 
         let x =res.data;
@@ -44,13 +60,13 @@ async function addtodom(){
 
         for (let i=0;i<x.length;i++) {
             if(x[i].category=='electronics'){
-                let append =`<li>${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
+                let append =`<li id="${x[i]._id}">${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
                 electronicul.innerHTML = electronicul.innerHTML + append;
             }else if(x[i].category =='skincare'){
-                let append =`<li>${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
+                let append =`<li id="${x[i]._id}">${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
                 skincareul.innerHTML = skincareul.innerHTML + append;
             }else{
-                let append =`<li>${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
+                let append =`<li id="${x[i]._id}">${x[i].category}-${x[i].sellingprice}-${x[i].productname} <button onclick="deleteitem('${x[i]._id}')">Delete</button></li>`;
                 fooditemul.innerHTML = fooditemul.innerHTML + append;
             }
         }
@@ -62,8 +78,10 @@ async function addtodom(){
 async function deleteitem(id) {
     // console.log(id,"InsideDeleteBtn");
     try{
-        await axios.delete(`https://crudcrud.com/api/743c894ac2734eeebbb09e0a1733a084/product/${id}`);
-       window.location.reload()
+         await axios.delete(`https://crudcrud.com/api/214b2eca9dac46359efe5b1ddf9e5660/product/${id}`);
+         let ToDelete = document.getElementById(`${id}`);
+         let parentElement = ToDelete.parentNode;
+         parentElement.removeChild(ToDelete);
     }catch(err){
         console.log(err);
     }
